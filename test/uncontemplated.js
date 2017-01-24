e7c9009d5c4c865cab9666e407d9e94c8a2fe6ef
Andreas Lind <andreas@one.com>
2017-01-24 23:41:59 +0100

    Support multiple assertions and middle-of-the-rocket assertions, allow arbitrary whitespace between before, after, and inside assertion strings.

--- test/uncontemplated.js
+++ test/uncontemplated.js
@@ -34,4 +34,54 @@ describe('uncontemplated', function () {
             expect(() => contemplate`123 to be less than 100`, 'to throw', 'expected 123 to be less than 100');
         });
     });
+
+    describe('with middle-of-the-rocket assertions', function () {
+        contemplate.addAssertion('<number> when incremented <assertion?>', (expect, subject, ...rest) => expect(subject + 1, ...rest));
+
+        it('should succeed', function () {
+            contemplate.addAssertion('<number> when incremented <assertion?>', (expect, subject, ...rest) => expect(subject + 1, ...rest));
+            contemplate`123 when incremented to equal 124`;
+        });
+
+        it('should fail with a diff', function () {
+            expect(() => contemplate`123 when incremented to equal 456`, 'to throw', 'expected 123 when incremented to equal 456');
+        });
+    });
+
+    describe('with multiple assertions on the same subject, separated by the word "and"', function () {
+        it('should succeed', function () {
+            contemplate`123 to be greater than 100 and to be a number`;
+        });
+
+        it('should fail with a diff', function () {
+            expect(
+                () => contemplate`123 to be greater than 100 and to be a string`,
+                'to throw',
+                "✓ expected 123 to be greater than 100 and\n" +
+                "⨯ expected 123 to be a string"
+            );
+        });
+    });
+
+    describe('with multiple assertions on the same subject, separated by the word "or"', function () {
+        it('should succeed', function () {
+            contemplate`123 to be less than 100 or to be a number`;
+        });
+
+        it('should fail with a diff', function () {
+            expect(
+                () => contemplate`123 to be less than 100 or to be a string`,
+                'to throw',
+                "⨯ expected 123 to be less than 100 or\n" +
+                "⨯ expected 123 to be a string"
+            );
+        });
+    });
+
+    it('should allow arbitrary whitespace around and inside the assertion strings', function () {
+        contemplate`
+            123 to be greater than 100
+            and to be         a number
+        `;
+    });
 });
